677276585293c8e9af67b72bbb6b87dc1c838115
Diego Serrano <dserrano@inti.gob.ar>
2022-02-07 17:52:33 -0300

    Ejemplo de angular clase 7

---
+++ portfolio/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Educacion } from 'src/educacion';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'portfolio';
+  nombreCompleto = "Juan Perez"
+
+  estudios: Educacion[] = []
+  intereses: String[] = ["Futbol","Ajedrez","Aves","Ferromodelismo","Atlanta"]
+  constructor() {
+    this.estudios.push(new Educacion("Bachiller","Escuela XX", "2000-2005"))
+    this.estudios.push(new Educacion("Analista de sistemas","Universidad Libre de Sistemas", "2006-2012"))
+    this.estudios.push(new Educacion("Tecnico en Redes","Instituto Formar", "2007-2012"))
+    this.estudios.push(new Educacion("Reposteria","Instituto Formar", "2020"))
+
+  }
+
+
+
+}
